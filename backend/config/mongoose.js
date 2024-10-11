012595000e9bf22c4be5dad112f6e81eaadcb016
Daniel Sebastian G. Quilatan <danielquilatan5@gmail.com>
2024-10-11 20:05:44 +0800

    Updated Mongoose String

--- backend/config/mongoose.js
+++ backend/config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://136.158.26.105/32/PIMSdb',
+mongoose.connect('mongodb+srv://mern:mern@cluster0.6mdyfjt.mongodb.net/PIMSdb?retryWrites=true&w=majority&appName=Cluster0',
 // mongoose.connect('mongodb://127.0.0.1:27017/PIMSdb', 
 {
 
