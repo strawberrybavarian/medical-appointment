947c81dcf5c2426d3d7e90c16b98dd71657a511c
Daniel Sebastian G. Quilatan <danielquilatan5@gmail.com>
2024-10-13 15:05:44 +0800

    Content Export Updated

--- frontend/src/ContentExport.js
+++ frontend/src/ContentExport.js
@@ -1,9 +1,9 @@
 const image = {
-    logo: "http://54.251.53.18/images/Molino-Polyclinic-Logo.png",
+    logo: "http://18.181.49.155/images/Molino-Polyclinic-Logo.png",
 };
 
 const ip ={
-    address: 'http://54.251.53.18'
+    address: 'http://18.181.49.155'
     // address: 'http://ec2-54-151-200-168.ap-southeast-1.compute.amazonaws.com:8000'
 
 };
